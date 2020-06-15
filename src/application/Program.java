dad4e56305ad4a7a06fd2eb8e1acf0410c3976d2
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-06-15 20:08:48 -0300

    better now

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,30 +12,31 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double limit = sc.nextDouble();
+		Account account = new Account(number, holder, balance, limit);
+		System.out.println();
+		
+		System.out.print("Enter amount fot withdraw: ");
+		limit = sc.nextDouble();
+	
 		try {
-			System.out.println("Enter account data");
-			System.out.print("Number: ");
-			int number = sc.nextInt();
-			sc.nextLine();
-			System.out.print("Holder: ");
-			String holder = sc.nextLine();
-			System.out.print("Initial balance: ");
-			double balance = sc.nextDouble();
-			System.out.print("Withdraw limit: ");
-			double limit = sc.nextDouble();
-			Account account = new Account(number, holder, balance, limit);
-			System.out.println();
-			
-			System.out.print("Enter amount fot withdraw: ");
-			limit = sc.nextDouble();
-			
 			account.withdraw(limit);
 			System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
 			
 		}catch (WithdrawException e) {
 			System.out.println("Withdraw error: " + e.getMessage());
 		}catch (RuntimeException e) {
-			System.out.println("Unexpected error" + e.getMessage());
+			System.out.println("Unexpected error");
 		}
 		
 		sc.close();
